cdedccbe48976133ba0e5a28439bfe289a51e186
sreeniii <sreenivaskanthamneni@gmail.com>
2016-12-21 21:12:17 -0600

    solution

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,42 @@
 $(function() {
 
-  // your code will go here
+  // https://www.codeschool.com/users/2840658.json
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2840658.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      populateWithCourses(response.courses.completed);
+    }
+  });
+
+  function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
